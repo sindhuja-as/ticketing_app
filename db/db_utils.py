d29df8ff7511e551224274a7bd888203f0087498
sindhuja-as <sindhuja.as268@gmail.com>
2025-12-25 21:28:59 +0530

    Update db_utils.py

--- db/db_utils.py
+++ db/db_utils.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 # DB_PATH = os.path.join("data", "tickets.db")
-DB_PATH = BASE_DIR / "data" / "tickets.db"
+DB_PATH = BASE_DIR \\"data" \\"tickets.db"
 
 def save_ticket(data):
     conn = sqlite3.connect(DB_PATH)
